147a953601cff382efc1523a12fc218077491af0
Reinoptland <reinoptland@gmail.com>
2021-10-29 09:33:21 +0200

    Implement debounce search with RXJS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log("Hi");
+import {
+  fromEvent,
+  debounceTime,
+  switchMap,
+  mergeMap,
+  tap,
+  catchError,
+} from "rxjs";
+import { fromFetch } from "rxjs/fetch";
+
+function displayError() {
+  const results = document.getElementById("results");
+  results.innerText = "Oh no, something went wrong!";
+}
+
+function displayNotFound() {
+  const results = document.getElementById("results");
+  results.innerText = "no results, sorry!";
+}
+
+function displayLoading() {
+  const results = document.getElementById("results");
+  results.innerText = "loading";
+}
+
+function displayResults(data) {
+  const results = document.getElementById("results");
+  results.innerHTML = "";
+  data.drinks.forEach((drink) => {
+    const drinkElement = document.createElement("div");
+    drinkElement.innerText = drink.strDrink;
+    results.appendChild(drinkElement);
+  });
+}
+
+const ONE_SECOND = 1000;
+const apiURL = "https://thecocktaildb.com/api/json/v1/1/search.php";
+
+const search = fromEvent(document.getElementById("search"), "keydown").pipe(
+  tap(() => displayLoading()),
+  debounceTime(ONE_SECOND),
+  switchMap((event) => fromFetch(`${apiURL}?s=${event.target.value}`)),
+  catchError(() => displayError()),
+  mergeMap((res) => res.json()),
+  tap((data) => (data.drinks ? displayResults(data) : displayNotFound()))
+);
+
+document.getElementById("search").addEventListener("focus", () => {
+  const subscription = search.subscribe();
+  document
+    .getElementById("search")
+    .addEventListener("blur", () => subscription.unsubscribe());
+});
